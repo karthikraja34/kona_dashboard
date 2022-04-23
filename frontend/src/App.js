cf32ac0ac3cee805a61da7ad515380daf74ebdbc
Karthik <rajakarthik131@gmail.com>
2022-04-23 14:19:34 +0530

    Display mental health score in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Box,
+  Container,
+  Grid,
+  GridItem,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import MentalHealthScoreCard from "./components/MentalHealthScoreCard";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box
+        bg={useColorModeValue("gray.100", "gray.900")}
+        py="20"
+        minH="100vh"
+        px={4}
+      >
+        <Container maxW="container.xl">
+          <Grid
+            templateRows="repeat(2, 1fr)"
+            templateColumns="repeat(5, 1fr)"
+            gap={4}
+          >
+            <GridItem colSpan={5}>
+              <MentalHealthScoreCard />
+            </GridItem>
+            <GridItem colSpan={2} bg="papayawhip" />
+            <GridItem colSpan={4} bg="tomato" />
+          </Grid>
+        </Container>
+      </Box>
     </div>
   );
 }
---
+++ frontend/src/components/MentalHealthScoreCard.js
@@ -0,0 +1,114 @@
+import React, { useState } from "react";
+import {
+  Box,
+  Flex,
+  Heading,
+  Select,
+  Button,
+  Stack,
+  Avatar,
+  CircularProgress,
+  Circle,
+  CircularProgressLabel,
+} from "@chakra-ui/react";
+import useFetch from "../hooks/useFetch";
+
+function MentalHealthScoreCard() {
+  const [timeline, setTimeline] = useState("today");
+  const [team, setTeam] = useState(null);
+  const { response } = useFetch("http://localhost:8000/api/v1/teams/");
+  const getURL = () => {
+    let url = `http://localhost:8000/api/v1/scores/?timeline=${timeline}`;
+    if (team) {
+      url = `${url}&team=${team}`;
+    }
+    return url;
+  };
+  const {
+    response: scoresResponse,
+    loading,
+    error,
+    refetch,
+  } = useFetch(getURL());
+
+  const getBorderColor = (score) => {
+    if (score > 70) {
+      return "green.500";
+    } else if (score > 40) {
+      return "yellow.500";
+    } else {
+      return "red.500";
+    }
+  };
+
+  return (
+    <div>
+      <Box
+        bg="white"
+        px={8}
+        py={10}
+        borderWidth="1px"
+        borderRadius="lg"
+        overflow="hidden"
+      >
+        <Flex justifyContent={"space-between"} alignItems="center">
+          <Heading as="h4" size="md">
+            Mental Health Scorecard
+          </Heading>
+          <Box display="flex">
+            <Select
+              placeholder="Select team"
+              defaultValue=""
+              onChange={(e) => setTeam(e.target.value)}
+            >
+              <option value="">All</option>
+              {response &&
+                response.results.map((team) => (
+                  <option key={team.id} value={team.id}>
+                    {team.name}
+                  </option>
+                ))}
+            </Select>
+
+            <Select
+              marginLeft={4}
+              defaultValue="today"
+              onChange={(e) => setTimeline(e.target.value)}
+            >
+              <option value="today">Today</option>
+              <option value="this_week">This Week</option>
+              <option value="this_month">This Month</option>
+            </Select>
+          </Box>
+        </Flex>
+
+        <Flex marginTop={10}>
+          <Stack direction="row" overflowX={"scroll"}>
+            {scoresResponse &&
+              scoresResponse.results.map((scoreData) => (
+                <Box textAlign="center" padding={3}>
+                  <CircularProgress
+                    color={getBorderColor(scoreData.score)}
+                    size="70px"
+                    value={scoreData.score}
+                  >
+                    <CircularProgressLabel>
+                      <Avatar name={scoreData.user.username} />
+                    </CircularProgressLabel>
+                  </CircularProgress>
+                  <h4 className="text-gray-500 font-semibold text-sm mt-2">
+                    {scoreData.user.username}
+                  </h4>
+                  <h5 className="text-gray-900 font-bold text-sm">
+                    {scoreData.score}
+                  </h5>
+                </Box>
+              ))}
+          </Stack>
+        </Flex>
+      </Box>
+    </div>
+  );
+}
+
+export default MentalHealthScoreCard;
