c4a356c901d4c1d58bb234a9769932159eff1303
Karthik <rajakarthik131@gmail.com>
2022-04-23 09:51:27 +0530

    Normalize model

--- kona_dashboard/checkins/models.py
+++ kona_dashboard/checkins/models.py
@@ -8,14 +8,31 @@ class DailyCheckIn(models.Model):
     RED = 1
     YELLOW = 2
     GREEN = 3
-    COLORS = (("Red", RED), ("Yellow", YELLOW), ("Green", GREEN))
+    COLORS = ((RED, "Red"), (YELLOW, "Yellow"), (GREEN, "Green"))
 
     elaboration = models.TextField(blank=True, null=True)
     emotion = models.CharField(null=True, max_length=255)
     platform = models.PositiveIntegerField(choices=PLATFORM, default=1)
     color = models.PositiveIntegerField(choices=COLORS)
-    organization = models.CharField("Organization Name", max_length=255)
-    team = models.CharField("Team Name", max_length=255)
-    username = models.CharField("Username", max_length=255)
+    team = models.ForeignKey("checkins.Team", on_delete=models.CASCADE)
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username + " is feeling " + self.get_color_display()
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=1024)
+    organization = models.ForeignKey("checkins.Organization", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Organization(models.Model):
+    name = models.CharField(max_length=1024)
+
+    def __str__(self):
+        return self.name
